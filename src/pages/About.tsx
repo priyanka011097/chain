22ecadc94170bd7f56d22e7ac61f6a5bf7885fde
priyanka011097 <priyankavilasshahasane@gmail.com>
2025-06-12 23:33:15 +0530

    added page title in about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,7 @@
 import PageTitle from "../components/PageTitle";
 
 const About = () => {
-  return (
+  return (<>
     <PageTitle
       topbutton="Chain Security"
       title1="Chain Security Is"
@@ -10,6 +10,23 @@ const About = () => {
       subtitle1="technology company"
       text="Specializes in developing blockchain analytics tools and investigating crypto fraud incidents worldwide."
     />
+    <PageTitle
+      title1="Our Unique Approach to Global"
+      subtitle1='Compliance'
+      highlight2="And"
+      subtitle2='Innovation'
+      text="We build and deliver original, localized solutions tailored to the needs of diverse markets, collaborating with institutions, regulators, and end users around the world."
+    />
+    <PageTitle
+      highlight1="Chainsecurity"
+      title2="tools make"
+      subtitle1='blockchain safer'
+      text="Enjoy a more private and secure internet, block malicious websites, stop web trackers, and monitor your data leaks all in one app."
+    />
+      <PageTitle
+      primaryButton="See All Features"
+     />
+  </>
   )
 };
 
